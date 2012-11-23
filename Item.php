40cbcac3252756141f0bcf4b8f37be689d4c4ebb
Jake Ottosson <jo222an@student.lnu.se>
2012-11-23 10:36:11 +0100

    Update
    Item.php

---
+++ Item.php
@@ -0,0 +1,24 @@
+<?php
+//Helper class
+class Item {
+	private $title;
+	private $url;
+	private $img;
+	
+	function __construct($titles, $urls, $imgs) {
+		$this->title = $titles;
+		$this->url = $urls;
+		$this->img = $imgs;
+	}
+	
+	public function Title() {
+		return $this->title;
+	}
+	public function Url() {
+		return $this->url;
+	}
+	public function Img() {
+		return $this->img;
+	}
+}
+?>
--- srTracker.php
+++ srTracker.php
@@ -1,27 +1,6 @@
 <?php
-//Helper class
-class Item {
-	private $title;
-	private $url;
-	private $img;
-	
-	function __construct($titles, $urls, $imgs) {
-		$this->title = $titles;
-		$this->url = $urls;
-		$this->img = $imgs;
-	}
-	
-	public function Title() {
-		return $this->title;
-	}
-	public function Url() {
-		return $this->url;
-	}
-	public function Img() {
-		return $this->img;
-	}
-}
-//srTracker class
+require_once('Item.php');
+
 class srTracker {
 	private $host;
 	private $username;
